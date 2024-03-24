2f67e5e490a7612ba53f211a67026761b58640de
RyTek <rytek1128@outlook.com>
2024-03-24 10:47:58 +0700

    Lab 2.5

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/", (req, res, next) => {
-  console.log("This always run!");
+  // console.log("This always run!");
   next();
 });
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In the middleware!");
-  res.send('<h1>The "Add Product" Page');
+  // console.log("In the middleware!");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
+  // console.log("In another middleware!");
   res.send("<h1>Hello from Express.js</h1>");
 });
 
